e3c51d3188ee49db9373d1b954bcc528ddee2426
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-05-17 10:12:18 +0530

    Committed!

--- Base_10_Ones_Complement.cpp
+++ Base_10_Ones_Complement.cpp
@@ -6,14 +6,21 @@ int main(){
     int number;
     cout<<"Provide the number: ";
     cin>>number;
-    int mask = 0, m = number;
-
+    int mask = 0, m = number, flag = 0;
+    if(m == 0 ){
+        flag = 1;
+    }
     while(m != 0 ){
         mask = mask<<1|1;
         m = m>>1;
     }
     int ans = (~number)&mask;
+    if(flag != 1){
     cout<<"One's complement(base-10): "<<ans<<endl;
+    }
+    else{
+        cout<<"One's complement(base-10): "<<0<<endl;
+    }
 
     return 0;
 }
