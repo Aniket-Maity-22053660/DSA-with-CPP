211fc7b572aa04e72c8e291d1d809ae4da0fdb38
Aniket <22053660@kiit.ac.in>
2025-05-20 16:07:47 +0000

    Committed!

---
+++ Array_Problems/sort1s2sAnd3s.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int> vec{1,2,0,2,0,2,0,2,0,1,0,2,1,2,1,2};
+    int left = 0,mid = 0;
+    int right = vec.size() - 1;
+
+    while(mid <= right){
+        if(vec[mid] == 0){
+            swap(vec[left], vec[mid]);
+            left++;
+            mid++;
+        }
+        else if(vec[mid] == 1){
+            mid++;
+        }
+        else{
+            swap(vec[mid], vec[right]);
+            right--;
+        }
+    }
+
+cout<<"0s, 1s and 2s sorted array: ";
+for(const auto &x: vec){
+    cout<<x<<" ";
+}
+cout<<endl;
+return 0;
+}
