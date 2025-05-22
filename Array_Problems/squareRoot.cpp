e2bd399225b33d945541ba5d6b3fdf17a6adf429
Aniket <22053660@kiit.ac.in>
2025-05-22 06:43:11 +0000

    Committed!

--- Array_Problems/squareRoot.cpp
+++ Array_Problems/squareRoot.cpp
@@ -2,11 +2,22 @@
 
 using namespace std;
 
+double precision(int num, int precision, double ans){
+    double factor = 1.00;
+    for(int i = 0 ; i < precision ; i++){
+        factor /= 10;
+        for(double j = ans ; j*j < num ; ans = j, j += factor);
+        //ans = ans - factor;
+        
+    }
+    return ans;
+}
+
 int main(){
     int num;
     cout<<"Enter the number: ";
     cin>>num;
-    int ans = -1;
+    double ans = -1;
     int left = 1, right = num;
     while(left <= right){
         int mid = left + (right - left)/2;
@@ -19,15 +30,15 @@ int main(){
             right = mid - 1;
         }
         else{
-            ans = left;
+            ans = mid;
             left = mid + 1;
         }
     }
-    if(ans != -1){
+    if(ans != -1)
+    {
+        ans = precision(num, 10, ans);
         cout<<"Square root of "<<num<<": "<<ans<<endl;
     }
-    else{
-        cout<<"Square root of "<<num<<": "<<ans<<endl;
-    }
+  
     return 0;
 }
