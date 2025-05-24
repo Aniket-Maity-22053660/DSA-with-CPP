1ffd15c944abfa87535893bdd0a8876bef9bc935
Aniket <22053660@kiit.ac.in>
2025-05-24 13:07:50 +0000

    Committed!

---
+++ strings_problems/checkPalindrome.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+char toLowerCase(char ch){
+    if(ch >= 'a' && ch <= 'z')
+    return ch;
+else{
+    return (ch - 'A' + 'a');
+}
+} 
+
+int main(){
+    string name;
+    bool ans = true;
+    cout<<"Enter the name: ";
+    cin>>name;
+
+    int start = 0, end = name.length() - 1;
+
+    while(start < end){
+        
+        if(toLowerCase(name[start]) == toLowerCase(name[end])){
+            start++;
+            end--;
+        }
+        else{
+            ans = false;
+            break;
+        }
+    }
+    if(ans){
+        cout<<"Yes, this name is a palindrome!"<<endl;
+    }
+    else{
+        cout<<"This name is not a palindrome!"<<endl;
+    }
+    return 0;
+}
