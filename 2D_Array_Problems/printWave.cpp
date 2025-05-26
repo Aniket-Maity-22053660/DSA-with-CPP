68a1e488ff6fe0b7319ddd706710132a8f8187fd
Aniket <22053660@kiit.ac.in>
2025-05-26 14:28:45 +0000

    Committed

---
+++ 2D_Array_Problems/printWave.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int arr[][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+
+    for(int i = 0 ; i < 4; i++){
+        for(int j = 0 ; j < (sizeof(arr)/sizeof(int))/4  ; j++){
+            if(i%2 != 0 ){
+                cout<<arr[2-j][i]<<" ";
+            }
+            else{
+                cout<<arr[j][i]<<" ";
+            }
+        }
+    }
+    cout<<endl;
+    return 0;
+}
