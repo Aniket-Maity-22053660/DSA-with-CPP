039d32138be8a6728862059c0fa15afffe2463f6
Aniket <22053660@kiit.ac.in>
2025-05-27 07:39:19 +0000

    Committed!

--- 2D_Array_Problems/rotate90.cpp
+++ 2D_Array_Problems/rotate90.cpp
@@ -2,12 +2,14 @@
 #include<vector>
 
 using namespace std;
-int main(){
-    vector<vector<int>> arr = {{1,2,3}, {4,5,6}, {7,8,9}};
-    vector<vector<int>> ans(arr[0].size(), vector<int>(arr.size()));
+int main()
+{
+    vector<vector<int>> arr = {{1,2,3}, {4,5,6}};
+    vector<int> row (arr.size());
+    vector<vector<int>> ans(arr[0].size(), row);
     for(int i = 0 ; i < arr.size() ; i++){
         for(int j = 0 ; j < arr[0].size() ; j++){
-            ans[(arr[0].size()-1)-j][i] = arr[i][j];
+            ans[j][(arr.size()-1)-i] = arr[i][j];
         }
     }
     
