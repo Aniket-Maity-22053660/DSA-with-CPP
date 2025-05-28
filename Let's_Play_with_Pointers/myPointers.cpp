908034b300e4c6c54b5cd8cddf636187cd7f7cef
Aniket <22053660@kiit.ac.in>
2025-05-28 06:13:05 +0000

    Committed!

--- Let's_Play_with_Pointers/myPointers.cpp
+++ Let's_Play_with_Pointers/myPointers.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main (){
-    int *ptr = NULL;
-    cout<<"Value of ptr: "<<*ptr<<endl;
+    /*int *ptr = NULL;
+    cout<<"Value of ptr: "<<*ptr<<endl;*/
+    int arr[10];
+    cout<<"Address of arr: "<<arr<<endl;
+    cout<<"Value of &arr: "<<&arr<<endl;
+    cout<<"Value of &arr[0]: "<<&arr[0]<<endl;
     return 0;
 }
