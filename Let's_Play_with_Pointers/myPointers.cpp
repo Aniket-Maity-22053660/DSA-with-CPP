dab08437725c92c83c216dbf9a84fab80b37d9ce
Aniket <22053660@kiit.ac.in>
2025-05-28 07:46:55 +0000

    Committed!

--- Let's_Play_with_Pointers/myPointers.cpp
+++ Let's_Play_with_Pointers/myPointers.cpp
@@ -9,5 +9,26 @@ int main (){
     cout<<"Address of arr: "<<arr<<endl;
     cout<<"Value of &arr: "<<&arr<<endl;
     cout<<"Value of &arr[0]: "<<&arr[0]<<endl;
+
+    int*ptr = arr;
+    cout<<sizeof(ptr)<<endl;
+    cout<<sizeof(*ptr)<<endl;
+    cout<<sizeof(&ptr)<<endl;
+    int a[20] = {1, 2, 3, 5};
+    cout<<&a<<endl;
+
+    char ch[] = "Aniket Maity";
+    ch[0] = 'a';
+
+    char *c = &ch[0];
+    cout<<"Value of c: "<<c<<endl;
+    char temp = 'A';
+    char* ptr1 = &temp;
+    cout<<ptr1<<endl;
+    char* ptr2 = "Aniket Maity";
+   // ptr2[0] = 'a';
+    cout<<"Risky: "<<ptr2<<endl;
+
+
     return 0;
 }
