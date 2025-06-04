5e8ad95a9c2974009a4eeb25633c57c9775ef994
Aniket <22053660@kiit.ac.in>
2025-06-04 05:43:17 +0000

    Committed!

--- ConceptOfLinkedList/DoublyLinkedList.cpp
+++ ConceptOfLinkedList/DoublyLinkedList.cpp
@@ -34,39 +34,82 @@ void insertAtPos(node* &head, int data, int pos){
     }
     int init = 1;
     node* tempPtr = head;
-    while(init < pos-1){
+    while(init < pos-1 && tempPtr != nullptr){
         tempPtr = tempPtr->next;
         init++;
     }
+    if(tempPtr != nullptr  && tempPtr->next != nullptr){
     node* ptr = tempPtr->next;
     node* temp = new node(data);
     ptr->prev = temp;
     temp->next = ptr;
     tempPtr->next = temp;
-}
-void del(node* &head, int data){
-    if(head == nullptr)
+    }
+    else{
+        insertAtTail(head, data);
+    }
     return;
-    if(head->data == data){
+}
+void del(node* &head, int data) {
+    if (head == nullptr) return;
+
+    // Case 1: delete head
+    if (head->data == data) {
         node* temp = head;
         head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        }
         delete temp;
         return;
     }
-    while(head->next->data != data){
-        head = head->next;
+
+    node* tempPtr = head;
+    // Traverse and find the node before the one to be deleted
+    while (tempPtr->next != nullptr && tempPtr->next->data != data) {
+        tempPtr = tempPtr->next;
     }
-    node* temp = head->next;
-    head->next = temp->next;
-    temp->next->prev = head;
+
+    if (tempPtr->next == nullptr) {
+        // Node with given data not found
+        return;
+    }
+
+    node* temp = tempPtr->next;
+    tempPtr->next = temp->next;
+    if (temp->next != nullptr) {
+        temp->next->prev = tempPtr;
+    }
     delete temp;
-    return;
 }
+
+
+void reverse(node* &head) {
+    if (head == nullptr || head->next == nullptr) return;
+
+    node* current = head;
+    node* temp = nullptr;
+
+    // Traverse and swap prev and next for all nodes
+    while (current != nullptr) {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
+    }
+
+    // After the loop, temp is the previous node of the new head
+    if (temp != nullptr) {
+        head = temp->prev;
+    }
+}
+
+
 void print(node* head){
     while(head != nullptr){
         cout<<head->data;
         if(head->next != nullptr)
-        cout<<", ";
+        {cout<<", ";}
     else{
         cout<<'\n';
     }
@@ -92,6 +135,12 @@ int main(){
     insertAtPos(head, 100, 3);
     print(head);
     insertAtPos(head, 99, 3);
+    print(head);
+    //reverse(head);
+    insertAtTail(head, 500);
+    print(head);
+    insertAtPos(head, 111, 1);
+    reverse(head);
     print(head);
     return 0;
 }
