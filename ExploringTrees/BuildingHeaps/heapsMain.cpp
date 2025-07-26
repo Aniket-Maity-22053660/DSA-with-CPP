af4234cc40eb39b08e1293214f6c527d67fa85db
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-07-26 20:04:02 +0000

    Committed!

--- ExploringTrees/BuildingHeaps/heapsMain.cpp
+++ ExploringTrees/BuildingHeaps/heapsMain.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
 #include<vector>
+#include<climits>
 
 using namespace std;
 
 class Heaps{
-    vector<int> arr(1, INT_MAX);
+    vector<int> Arr {INT_MAX};
     int size = 0;
     void swap(int parent, int index){
-         int temp = arr[parent];
-         arr[parent] = arr[index];
-         arr[index] = temp;
+         int temp = Arr[parent];
+         Arr[parent] = Arr[index];
+         Arr[index] = temp;
     }
     public:
     void insert(int data){
         size = size + 1;
-        arr.push_back(data);
+        Arr.push_back(data);
         int index = size;
         while(index > 1){
             int parent = index / 2;
-            if(arr[parent] < arr[index]){
+            if(Arr[parent] < Arr[index]){
                 swap(parent, index);
                 index = parent;
             }else{
@@ -28,9 +29,54 @@ class Heaps{
     }
     void print(){
         vector<int>::iterator it;
-        for(it = arr.begin() + 1 ; it != arr.end() ; it++){
-            cout<<*it<<" ";
+        for(int i = 1 ; i <= size ; i++){
+            cout<<Arr[i]<<" ";
         }
         cout<<'\n';
     }
+    void deleteElement(int data){
+        int index = -1;
+        for(int i = 1 ; i <= size ; i++){
+            if(Arr[i] == data){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            return;
+        }
+        swap(index, size);
+        size = size - 1;
+        while((2*index <= size && 2*index + 1 <= size) && (Arr[index] < Arr[2*index] || Arr[index] < Arr[2*index + 1])){
+            if(Arr[index] < Arr[2*index]){
+                swap(index, 2*index);
+                index = 2*index;
+            }else if(Arr[index] < Arr[2*index + 1]){
+                swap(index, 2*index + 1);
+                index = 2*index + 1;
+            }
+        }
+    }
 };
+
+int main(){
+    int size, element;
+    cout<<"Enter the elements to be entered in the heap: ";
+    cin>>size;
+    Heaps* objHeap = new Heaps();
+    for(int i = 0 ; i < size ; i++){
+        cout<<"Element"<<(i+1)<<": ";
+        cin>>element;
+        objHeap->insert(element);
+    }
+    objHeap->print();
+    cout<<'\n';
+    cout<<"Enter the element you want to delete: ";
+    cin>>element;
+    objHeap->deleteElement(element);
+    cout<<"After deleting an element: ";
+    objHeap->deleteElement(element);
+    objHeap->print();
+    cout<<'\n';
+    return 0;
+}
